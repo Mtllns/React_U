f4cf4cb55e26122f09e444e05d3e3970218acead
Mtllns <miquelmtllns@pm.me>
2020-07-10 12:47:44 +0200

    Objetos literales

---
+++ 02-intro-javascript/src/bases/03-object-lite.js
@@ -0,0 +1,21 @@
+
+
+const persona = {
+    nombre: 'Tony',
+    apellido: 'Stark',
+    edad: 45,
+    direccion: {
+        ciudad: 'New York',
+        zip: 55879,
+        lat: 14.589,
+        lng: 34.89745
+    }
+};
+
+// console.table( persona );
+
+const persona2 = { ...persona };
+persona2.nombre = 'Peter';
+
+console.log( persona );
+console.log( persona2 );
