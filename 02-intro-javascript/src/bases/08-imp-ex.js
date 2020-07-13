93e8d7ca3e280ec227afbb8489c7dcbb7c60fd5c
Mtllns <miquelmtllns@pm.me>
2020-07-13 15:14:55 +0200

    Promesas

--- 02-intro-javascript/src/bases/08-imp-ex.js
+++ 02-intro-javascript/src/bases/08-imp-ex.js
@@ -11,13 +11,15 @@ import heroes, { owners } from '../data/heroes';
 //     }
 // }
 
-console.log( owners );
+// console.log( owners );
 
 const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
 
-console.log( getHeroeById(2) );
+// console.log( getHeroeById(2) );
 
 // find?, filter
 const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
 
-console.log( getHeroesByOwner('Marvel') );
+// console.log( getHeroesByOwner('Marvel') );
+
+export default getHeroeById;
