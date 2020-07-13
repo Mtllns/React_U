62fb9b52ea77cd4ef5e4b317d82b59d3452100ad
Mtllns <miquelmtllns@pm.me>
2020-07-13 12:11:20 +0200

    Multi expo y expor default

---
+++ 02-intro-javascript/src/bases/08-imp-ex.js
@@ -0,0 +1,23 @@
+import heroes, { owners } from '../data/heroes';
+
+
+// const getHeroeById = (id) => {
+//     return heroes.find( (heroe) ) => {
+//         if ( heroe.id === id ) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     }
+// }
+
+console.log( owners );
+
+const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
+
+console.log( getHeroeById(2) );
+
+// find?, filter
+const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
+
+console.log( getHeroesByOwner('Marvel') );
