c0f38c6f5c457a68de469a1cb4564a2a56eded2f
Mtllns <miquelmtllns@pm.me>
2020-07-21 13:20:12 +0200

    DefaultProps

--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -2,14 +2,14 @@
  import PropTypes from 'prop-types';
 //  import React, { Fragment } from 'react';
 
-const PrimeraApp = ({ saludo }) => {
+const PrimeraApp = ({ saludo, subtitulo }) => {
 
 
     return (
         <>
             <h1>  { saludo } </h1>
             {/* <pre> { JSON.stringify( saludo, null, 3 ) } </pre> */}
-            <p>Mi primera aplicación</p>
+            <p> { subtitulo } </p>
 
         </>
     );
@@ -20,5 +20,8 @@ PrimeraApp.propTypes = {
     saludo: PropTypes.string.isRequired
 }
 
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo'
+}
 
 export default PrimeraApp;
