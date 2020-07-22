d66679dfeaebad041f0c1ee31ed2143a08619dbd
Mtllns <miquelmtllns@pm.me>
2020-07-22 11:43:50 +0200

    Componente CounterApp

--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PrimeraApp from './PrimeraApp';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 import './index.css';
 
@@ -8,4 +9,6 @@ import './index.css';
 
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp  saludo="Hola Mundo" /> , divRoot );
+ReactDOM.render( <CounterApp value={ 100 } />, divRoot );
+
+// ReactDOM.render( <PrimeraApp  saludo="Hola Mundo" /> , divRoot );
