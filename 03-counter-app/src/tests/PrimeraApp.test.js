fb28f16bd7241c9d0a2588490ca216f0b0c463c0
Mtllns <miquelmtllns@pm.me>
2020-07-28 12:55:47 +0200

    Enzyme - Testing unit

--- 03-counter-app/src/tests/PrimeraApp.test.js
+++ 03-counter-app/src/tests/PrimeraApp.test.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import '@testing-library/jest-dom';
+import { shallow } from 'enzyme';
+
 import PrimeraApp from "../PrimeraApp";
 
 
 describe('Pruebas en <PrimeraApp />', () => {
     
-    test('debe mostrar el mensaje "Hola, soy Goku"', () => {
-       
+    // test('debe mostrar el mensaje "Hola, soy Goku"', () => {
+    //     const saludo = 'Hola, soy Goku';
+    //     const { getByText } = render( <PrimeraApp saludo={ saludo } /> );
+    //     expect( getByText(saludo) ).toBeInTheDocument();
+    // });
+
+    test('debe mostrar <PrimeraApp /> cosrrectamente ', () => {
+
         const saludo = 'Hola, soy Goku';
+        const wrapper = shallow( <PrimeraApp saludo={ saludo } /> )
 
-        const { getByText } = render( <PrimeraApp saludo={ saludo } /> );
-
-        expect( getByText(saludo) ).toBeInTheDocument();
+        expect( wrapper ).toMatchSnapshot();
 
     });
+    
     
 });
