9feb4fe9c87a382dd2dd93c76c34c841a597ad91
Mtllns <miquelmtllns@pm.me>
2020-07-30 15:29:50 +0200

    Mostrar títulos de las img

--- 04-gif-expert-app/src/components/GifGrid.js
+++ 04-gif-expert-app/src/components/GifGrid.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-    const [count, setCount] = useState(0);
+    const [images, setImages] = useState([]);
 
     useEffect( () => {
         getGifs();
@@ -23,6 +24,7 @@ export const GifGrid = ({category}) => {
         })
 
         console.log(gifs);
+        setImages( gifs );
 
     }
 
@@ -30,10 +32,16 @@ export const GifGrid = ({category}) => {
     return (
         <div>
             <h3>{ category }</h3>
-            <h3>{ count }</h3>
-            <button onClick={ () => setCount( count + 1 ) } >
-                    Notification
-            </button>
+            
+            {
+                images.map( img => (
+                    <GifGridItem 
+                        key={ img.id }
+                        { ...img }
+                    />
+                ))
+            }
+            
         </div>
     )
 }
